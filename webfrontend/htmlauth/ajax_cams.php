71853bd649aaf66d6308128c56994e16c5913884
ingenarius <ingenarius@poiss.priv.at>
2018-09-10 13:17:27 +0200

    Add files via upload

--- webfrontend/htmlauth/ajax_cams.php
+++ webfrontend/htmlauth/ajax_cams.php
@@ -11,29 +11,12 @@ $ds_port = $cfg['DISKSTATION']['PORT'];
 $ds_cids = $cfg['DISKSTATION']['CIDS'];
 
 //Get camera data
-// $myfile = fopen("$lbpdatadir/cameras.dat", "r") or die("Unable to open file!");
-// $data = fread($myfile,filesize("$lbpdatadir/cameras.dat"));
-// fclose($myfile);
-// $order   = array("\r\n", "\n", "\r");
-// $replace = '<br />';
-// $cams = str_replace($order, $replace, $data);
+$myfile = fopen("$lbpdatadir/cameras.dat", "r") or die("Unable to open file!");
+$data = fread($myfile,filesize("$lbpdatadir/cameras.dat"));
+fclose($myfile);
+$order   = array("\r\n", "\n", "\r");
+$replace = '<br />';
+$cams = str_replace($order, $replace, $data);
 
-// echo "<span class=\"hint\">".$cams."</span>";
-
-$cameras = array();
-$camstring = "";
-// $handle = fopen("../../data/cameras.dat", "r") or die("Unable to open file!");
-$cameras = file("../../data/cameras.dat", FILE_IGNORE_NEW_LINES);
-// while (($cam = fgets($handle)) !== false)
-    // array_push($cameras, $cam);
-// fclose($handle);
-
-foreach($cameras as $cam) {
-	// echo "RAW: $cam";
-	list($cid, $cmodel) = explode(':', $cam);
-	$camstring = $camstring."<input type=\"checkbox\" name=\"cam$cid\" value=\"$cmodel\"><br>";
-	//echo "ID: $cid, Model: $cmodel";
-}
-echo "\n";
-echo $camstring;
-echo "\n";
+echo "<span class=\"hint\">".$cams."</span>";
+?>
