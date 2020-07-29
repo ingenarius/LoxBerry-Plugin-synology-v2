3fe1d8176831ae63d7fab91421c2731c8d5a30f5
ingenarius <ingenarius@poiss.priv.at>
2020-07-29 12:54:32 +0200

    Update udp_client.py

--- bin/udp_client.py
+++ bin/udp_client.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 
 import socket
 
 UDP_IP="127.0.0.1"
 UDP_PORT=5000
-MESSAGE="archangel says: go to sleep"
+MESSAGE="miniserver says: go to sleep"
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
